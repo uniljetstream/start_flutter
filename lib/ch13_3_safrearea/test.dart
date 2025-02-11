ef0b16110c73c640734797c6f2811d9004666359
wins_linux <trn01@naver.com>
2025-02-11 20:59:48 +0900

    ch13_3_safearea

---
+++ lib/ch13_3_safrearea/test.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  List<Widget> getWidgets() {
+    List<Widget> widgets = [];
+    for (var i = 0; i < 100; i++) {
+      widgets.add(ListTile(
+        title: Text('Hello World Item $i'),
+      ));
+    }
+    return widgets;
+  }
+
+  @override
+  Widget build(BuildContext conttext) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+            body: SafeArea(
+                child: SingleChildScrollView(
+                    child: Column(children: getWidgets(),)))
+                /*
+                SingleChildScrollView(
+                  child: Colum (children: getWidgets(),)))
+                 */
+        )
+    );
+  }
+}
