97518ecf2a5073c4aa9760c9d4e1d9b38482f919
wins_linux <trn01@naver.com>
2025-03-30 21:21:28 +0900

    ch23_4_sqflite

--- lib/ch23_4_sqflite/test.dart
+++ lib/ch23_4_sqflite/test.dart
@@ -23,7 +23,7 @@ class User {
   String? name;
   String? address;
 
-  Map<String, Object?> toMap() {
+  Map<String, Object?> toMap() {    //db에 저장할 때 객체의 속성값을 Map 객체로 만듬.
     var map = <String, Object?>{"name": name, "address": address};
     if (id != null) {
       map["id"] = id;
@@ -33,7 +33,7 @@ class User {
 
   User.fromData(this.name, this.address);
 
-  User.fromMap(Map<String, Object?> map) {
+  User.fromMap(Map<String, Object?> map) { //데이터베이스의 Map 데이터를 객체의 속성에 대입.
     id = map["id"] as int;
     name = map['name'] as String;
     address = map['address'] as String;
@@ -54,16 +54,23 @@ class NativePluginWidgetState extends State<NativePluginWidget> {
 
   var db;
 
-  _createTable() async {
-    db = await openDatabase(
+  _createTable() async {  //db 생성
+    db = await openDatabase(  //version : 개발자 지정 db 버전.
         "my_db.db", version: 1, onCreate: (Database db, int version) async {
-      await db.execute('''
+          //onCreate : 앱 설치 후 openDatabase()로 처음 데이터베이스를 이용할 때 딱 한번 호출. 주로 테이블 생성 작업을 작성
+      await db.execute('''  
         CREATE TABLE User(
         id INTEGER PRIMARY KEY,
         name TEXT,
         address Text)
       ''');
+      //execute()는 반환값이 없다. => 주로 테이블을 다룰 때 사용.
+      //모든 SQL문을 실행 할 수 있지만 반환값이 없음. 작업별함수를 별도로 제공
+      //이 밖에도 sql문을 실행 할 수 있는 함수
+      //:rawQuery(), rawInsert(), rawUpdate(), rawDelete()
+      //각각 SELECT, INSERT, UPDATE, DELETE문 실
     }, onUpgrade: (Database db, int oldVersion, int newVersion) {});
+    //openDatabase의 version에 변경이 생기면 실행.
   }
 
   int lastId = 0;
@@ -71,11 +78,11 @@ class NativePluginWidgetState extends State<NativePluginWidget> {
   insert() async {
     lastId++;
     User user = User.fromData('name$lastId', 'seoul$lastId');
-    lastId = await db.insert("User", user.toMap());
+    lastId = await db.insert("User", user.toMap()); //User db에 user를 Map객체로 바꿔 삽입.
     print('${user.toMap()}');
   }
 
-  update() async {
+  update() async {  //마지막에 저장된 것의 lastId를 -1로 수정함.
     User user = User.fromData('name${lastId - 1}', 'seoul${lastId - 1}');
     await db.update("User", user.toMap(), where: 'id=?', whereArgs: [lastId]);
   }
@@ -88,16 +95,16 @@ class NativePluginWidgetState extends State<NativePluginWidget> {
   query() async {
     List<Map> maps = await db.query(
       'User',
-      columns: ['id', 'name', 'address'],
+      columns: ['id', 'name', 'address'], //User db에서 id, name, address에 조회
     );
-    List<User> users = List.empty(growable: true);
-    maps.forEach((element) {
+    List<User> users = List.empty(growable: true);  //User 객체를 저장할 빈 리스트 생성
+    maps.forEach((element) {  //조회된 각 행에 대해 User 객체로 반복하면서 user 리스트에 추가
       users.add(User.fromMap(element as Map<String, Object?>));
     });
-    if (maps.length > 0) {
+    if (maps.length > 0) {  //조회된 결과가 있는 경우 print문 실행
       print('select: ${maps.first}');
     }
-    users.forEach((user) {
+    users.forEach((user) {  //user리스트에 있는 모든 User객체에 대해 모든 name 속성 출력
       print('${user.name}');
     });
   }
